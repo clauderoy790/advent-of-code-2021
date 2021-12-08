5d5ead95088e925498de376410f8404358ebc084
claude_roy <Claude.Roy@mheducation.com>
2021-12-07 22:25:35 -0600

    Add map example to calculate easily

--- cmd/day6/main.go
+++ cmd/day6/main.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 )
 
-var days = 18
+var days = 256
 
 func main() {
 	strs := helpers.GetInputStrings("day6")
@@ -16,7 +16,55 @@ func main() {
 		nbs = append(nbs, n...)
 	}
 	// simulateAsync(nbs)
-	simulateMap(nbs)
+	simulateWorking(nbs)
+	// simulateMap(nbs)
+}
+
+func simulateWorking(nbs []int) {
+	m := make(map[int]int)
+	// init
+	for i := 0; i <= 8; i++ {
+		m[i] = 0
+	}
+	for _, nb := range nbs {
+		m[nb]++
+	}
+
+	for day := 0; day < days; day++ {
+		newM := initMap()
+		for k,v := range m {
+			if k == 0 {
+				newM[8] += v
+				newM[6] += v
+			} else {
+				newM[k-1] += v
+			}
+		}
+		m = copyMap(newM)
+	}
+
+	var count uint64 = 0
+	for _,v := range m {
+		count += uint64(v)
+	}
+	fmt.Println("RESP IS: ",count)
+
+}
+
+func initMap() map[int]int {
+	newM := make(map[int]int)
+	for i:=0;i<=8;i++ {
+		newM[i] = 0
+	}
+	return newM
+}
+
+func copyMap(m map[int]int) map[int]int {
+	newM := make(map[int]int)
+	for k, v := range m {
+		newM[k] = v
+	}
+	return newM
 }
 
 var count uint64
@@ -49,7 +97,7 @@ func simulateM(startDay int, startVal int, wg *sync.WaitGroup) {
 		startVal--
 		day++
 	}
-	
+
 	var ct uint64 = 0
 	additions := (days - day) / 7
 	for i := 0; i <= additions; i++ {
@@ -58,11 +106,12 @@ func simulateM(startDay int, startVal int, wg *sync.WaitGroup) {
 		wg.Add(1)
 		ct++
 	}
-	fmt.Println("CT: ",ct)
+	fmt.Println("CT: ", ct)
 	inc(ct)
 }
 
 var mu sync.Mutex
+
 func inc(val uint64) {
 	mu.Lock()
 	defer mu.Unlock()
