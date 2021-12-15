9511593a56f25819b318c3bdf19e0fb275db5580
claude_roy <Claude.Roy@mheducation.com>
2021-12-14 22:23:07 -0500

    start part 2

--- cmd/day14/main.go
+++ cmd/day14/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"clauderoy790/advent-of-code-2021/helpers"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
 var template string
-var instructions []instruction
+var instructions map[string]string
 
 func main() {
 	strs := helpers.GetInputStrings("day14")
 	tp := parseInput(strs)
-	part1(40, tp)
+	part1(10, tp)
 }
 
 var steps = 0
@@ -24,33 +25,47 @@ func part1(nbSteps int, template string) {
 	for i := 0; i < steps; i++ {
 		toApply := []instruction{}
 		temp := newStr
-		for j := 0; len(temp) >= 2; j += 2 / 2 {
-			if ins := getInstruction(temp[0:2]); ins != nil {
-				toApply = append(toApply, instruction{from: ins.from, to: ins.to, index: j + 2/2})
+		// iterate string
+		pattern := ""
+		longerTo := ""
+		for j := 0; len(temp) >= 2; j++ {
+			from, to, err := getInstruction(temp)
+			if err == nil {
+				pattern += temp[0:2]
+				longerTo += to
+				if _, ok := instructions[pattern]; !ok {
+					fmt.Printf("adding new instruction: %v: %v\n", pattern, longerTo)
+					addInstruction(pattern, longerTo)
+				}
+				toApply = append(toApply, instruction{from: from, to: to, index: j + len(from)/2})
+			} else {
+				pattern = ""
+				longerTo = ""
 			}
-			temp = temp[2/2:]
+			temp = temp[len(from)/2:]
 		}
-		// toApply = sortApply(toApply, newStr)
+
 		//apply
 		nbInserted := 0
 		for _, ins := range toApply {
 			newStr = insertStringAt(newStr, ins.to, ins.index+nbInserted)
 			nbInserted += len(ins.to)
 		}
-		fmt.Printf("After step %v: %v\n", (i + 1), "coucou")
+		// fmt.Printf("After step %v: %v\n", (i + 1), newStr)
+		fmt.Printf("After step %v\n", (i + 1))
 	}
 
 	m := findMostCommonLetter(newStr)
 	if len(m) != 2 {
 		panic("invalid common letters length")
 	}
-	most, least := -1, -1
+	most, least := uint64(1), uint64(1)
 	for _, v := range m {
-		if most == -1 {
+		if most == uint64(1) {
 			most = v
 			continue
 		}
-		if least == -1 {
+		if least == uint64(1) {
 			least = v
 			continue
 		}
@@ -65,23 +80,33 @@ func part1(nbSteps int, template string) {
 	fmt.Println("P1: ", (most - least))
 }
 
-func getInstruction(str string) *instruction {
-	for _, ins := range instructions {
-		if ins.from == str {
-			return &ins
-		}
+var maxInstructionLength = 0
+
+func addInstruction(key, val string) {
+	if len(key) > maxInstructionLength {
+		maxInstructionLength = len(key)
 	}
-	return nil
+	instructions[key] = val
 }
 
-func getSmallestTemplateLength() int {
-	smallest := 100000
-	for _, ins := range instructions {
-		if len(ins.from) < smallest {
-			smallest = len(ins.from)
+func getInstruction(str string) (string, string, error) {
+	max := len(str)
+	if max%2 != 0 {
+		max--
+	}
+
+	if maxInstructionLength < max {
+		max = maxInstructionLength
+	}
+	for keyLen := max; keyLen >= 2; keyLen -= 2 {
+		key := str[:keyLen]
+		for k, v := range instructions {
+			if key == k {
+				return k, v, nil
+			}
 		}
 	}
-	return smallest
+	return "", "", errors.New("failed to find")
 }
 
 func insertStringAt(str string, sub string, pos int) string {
@@ -96,38 +121,15 @@ func insertStringAt(str string, sub string, pos int) string {
 	return string(temp)
 }
 
-func sortApply(ins []instruction, str string) []instruction {
-	sorted := []instruction{}
-	for len(ins) > 0 {
-		smallest := 10000
-		index := 0
-		for i, in := range ins {
-			ind := strings.Index(str, in.from)
-			if ind == -1 {
-				panic("not supped to has -1 index here")
-			}
-			if ind < smallest {
-				smallest = ind
-				index = i
-			}
-		}
-
-		sorted = append(sorted, ins[index])
-		ins = append(ins[:index], ins[index+1:]...)
-	}
-	// fmt.Println("sorted: ", sorted)
-	return sorted
-}
-
-func findMostCommonLetter(str string) map[rune]int {
-	m := make(map[rune]int)
-	most := make(map[rune]int)
+func findMostCommonLetter(str string) map[rune]uint64 {
+	m := make(map[rune]uint64)
+	most := make(map[rune]uint64)
 	for _, ru := range str {
 		m[ru]++
 	}
 
 	r := ' '
-	c := 0
+	c := uint64(0)
 	// find most common
 	for k, v := range m {
 		if v > c {
@@ -138,7 +140,7 @@ func findMostCommonLetter(str string) map[rune]int {
 	most[r] = c
 
 	r = ' '
-	c = 1000000
+	c = uint64(10000000000000000000)
 	// find least
 	for k, v := range m {
 		if v < c {
@@ -152,6 +154,7 @@ func findMostCommonLetter(str string) map[rune]int {
 
 func parseInput(strs []string) string {
 	template = strings.TrimSpace(strs[0])
+	instructions = make(map[string]string)
 	for _, str := range strs {
 		if len(str) == 0 || !strings.Contains(str, "->") {
 			continue
@@ -161,10 +164,9 @@ func parseInput(strs []string) string {
 		for i := range sp {
 			sp[i] = strings.TrimSpace(sp[i])
 		}
-
-		ins := instruction{from: sp[0], to: sp[1]}
-		instructions = append(instructions, ins)
+		addInstruction(sp[0], sp[1])
 	}
+	fmt.Println("instructions: ", instructions)
 	return template
 }
 
